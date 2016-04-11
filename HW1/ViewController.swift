f83ce10062b970ee019f5a5162fa133ffa66d6de
safarsi <safarsi@ucsd.edu>
2016-04-10 23:00:41 -0700

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Sara Farsi on 4/10/16.
+//  Copyright © 2016 Sara Farsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
